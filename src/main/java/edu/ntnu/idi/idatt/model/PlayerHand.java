119cf19b9df419f755d6e67b3e9265b2c98f9a71
MatejaV2005 <matejav@stud.ntnu.no>
2025-03-13 12:57:44 +0100

    made PlayerHand class with a checkFlush method (not used for now)

--- src/main/java/edu/ntnu/idi/idatt/model/PlayerHand.java
+++ src/main/java/edu/ntnu/idi/idatt/model/PlayerHand.java
@@ -1,7 +1,9 @@
 package edu.ntnu.idi.idatt.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlayerHand {
   private List<PlayingCard> hand;
@@ -14,8 +16,20 @@ public class PlayerHand {
     hand.add(card);
   }
 
-  public void checkFlush() {
+  public List<List<PlayingCard>> checkFlush() {
+    return hand.stream()
+        .collect(Collectors.groupingBy(PlayingCard::getType))  // Group by suit
+        .entrySet().stream()
+        .filter(entry -> entry.getValue().size() >= 5)  // checkc the value (list) of the entry, keep only with 5+ cards
+        .map(entry -> entry.getValue().stream()         // Extract the list of cards, no need for the suit
+            .sorted(Comparator.comparingInt(PlayingCard::getValue).reversed())
+            .limit(5)  // Keep the top 5 highest-ranked cards
+            .collect(Collectors.toList()))  // Collect as a list
+        .collect(Collectors.toList());  // Return All flushes
+  }
 
+  public void addCardToHand(PlayingCard card) {
+    hand.add(card);
   }
 
   public List<PlayingCard> getHand() {
